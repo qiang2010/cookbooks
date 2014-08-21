548c99ce567668df370910458a612c804f98ced8
qiang2010 <qiangji1991@gmail.com>
2014-08-21 10:18:35 +0800

    delete some cookbooks

--- web/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'web'
-maintainer       ''
-maintainer_email ''
-license          ''
-description      'Installs/Configures web'
-long_description 'Installs/Configures web'
-version          '0.1.0'
-
